e56107a537b3db4beb62c54daa206be575271ce2
student <student@mbp-stud-10.local>
2020-02-16 23:39:40 -0600

    first commit

--- MAS157ControlFunVanA/ViewController.swift
+++ MAS157ControlFunVanA/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var numberField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
