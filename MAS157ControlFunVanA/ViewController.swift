1aead1371084b863a6eb62f367704940e1a5d291
student <student@mbp-stud-10.local>
2020-02-16 22:50:12 -0600

    Initial Commit

---
+++ MAS157ControlFunVanA/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MAS157ControlFunVanA
+//
+//  Created by student on 2/16/20.
+//  Copyright © 2020 vanderson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
